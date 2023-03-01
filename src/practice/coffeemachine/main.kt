02a1832215e7f609219b326f5342e9aa7f5a9ad1
Nikita <medvedhopod@gmail.com>
2023-03-01 18:29:10 +0200

    stage3

--- src/practice/coffeemachine/main.kt
+++ src/practice/coffeemachine/main.kt
@@ -1,11 +1,16 @@
 package practice.coffeemachine
 
-
 fun main() {
+    println("Write how many ml of water the coffee machine has:")
+    val water = Integer.valueOf(readLine())
+    println("Write how many ml of milk the coffee machine has:")
+    val milk = Integer.valueOf(readLine())
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val coffeeBeans = Integer.valueOf(readLine())
     println("Write how many cups of coffee you will need:")
     val cups = Integer.valueOf(readLine())
-    println("For $cups cups of coffee you will need:\n" +
-            "${cups * 200} ml of water\n" +
-            "${cups * 50} ml of milk\n" +
-            "${cups * 15} g of coffee beans")
+
+    if (200 * cups <= water && 50 * cups <= milk && 15 * cups <= coffeeBeans) {
+        println("Yes, I can make that amount of coffee")
+    } else {println("No, I can make only N cups of coffee")}
 }
