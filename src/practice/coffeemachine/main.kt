f3c2a9054b6622dbd1d68b643955a7ac54e966f0
Nikita <medvedhopod@gmail.com>
2023-03-01 16:57:49 +0200

    stage1

---
+++ src/practice/coffeemachine/main.kt
@@ -0,0 +1,14 @@
+package practice.coffeemachine
+
+
+class Stage1{
+    init {
+        println("Starting to make a coffee in NIX office\nGrinding coffee beans\nBoiling water\n" +
+                "Mixing boiled water with crushed coffee beans\nPouring coffee into the cup\n" +
+                "Pouring some milk into the cup\nCoffee is ready! Go to work!")
+    }
+}
+
+fun main() {
+    Stage1()
+}
