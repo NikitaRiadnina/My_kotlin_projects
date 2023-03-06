6681cbee117cc36091e0cd5ce7a130c346931963
Nikita <medvedhopod@gmail.com>
2023-03-06 14:15:23 +0200

    stage3

--- src/practice/coffeemachine/main.kt
+++ src/practice/coffeemachine/main.kt
@@ -1,16 +1,22 @@
 package practice.coffeemachine
 
+
 fun main() {
     println("Write how many ml of water the coffee machine has:")
-    val water = Integer.valueOf(readLine())
+    var cupsCan = Integer.valueOf(readLine()) / 200
     println("Write how many ml of milk the coffee machine has:")
-    val milk = Integer.valueOf(readLine())
+    cupsCan = find_min(cupsCan, Integer.valueOf(readLine()) / 50)
     println("Write how many grams of coffee beans the coffee machine has:")
-    val coffeeBeans = Integer.valueOf(readLine())
+    cupsCan = find_min(cupsCan, Integer.valueOf(readLine()) / 15)
     println("Write how many cups of coffee you will need:")
-    val cups = Integer.valueOf(readLine())
+    val cupsNeed = Integer.valueOf(readLine())
 
-    if (200 * cups <= water && 50 * cups <= milk && 15 * cups <= coffeeBeans) {
-        println("Yes, I can make that amount of coffee")
-    } else {println("No, I can make only N cups of coffee")}
+    if (cupsNeed == cupsCan) { println("Yes, I can make that amount of coffee")}
+    else if (cupsNeed > cupsCan) { println("No, I can make only $cupsCan cups of coffee")}
+    else { println("Yes, I can make that amount of coffee (and even ${cupsCan - 1} more than that")}
+}
+
+
+fun find_min(a: Int, b: Int): Int {
+    return if (a > b) { b } else { a }
 }
